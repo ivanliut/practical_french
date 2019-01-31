33333a9ee7530db02f1209f730d4ac891ba676b4
Ivan Liutko <ivanliutko@gmail.com>
2019-02-01 00:06:17 +0200

    init project & add ngx-bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'pr-fr';
+    title$: Observable<any>;
+
+    constructor(private http: HttpClient) {
+        let headers: HttpHeaders = new HttpHeaders().set('Content-Type', 'application/json');
+        headers = headers.append('Content-Type', 'application/json');
+        this.title$ = this.http.post(
+            'https://hlo2oz04nb.execute-api.us-east-2.amazonaws.com/dev/compare-yourself',
+        {
+            'age': 27
+        },
+        { headers }
+    );
+        console.log(this.title$);
+    }
+
 }
